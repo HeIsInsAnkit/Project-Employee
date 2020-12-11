f07695d87b75adbe9fdb5972d01c83676ef70c1f
Ankit Gupta <ankitgupta10121700@gmail.com>
2020-12-11 17:09:09 +0545

    update

--- src/main/java/com/iot83/app/Main/AppApplication.java
+++ src/main/java/com/iot83/app/Main/AppApplication.java
@@ -1,9 +1,9 @@
-package com.iot83.app.Main;
+package com.iot83.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.iot83.app"})
+@SpringBootApplication
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
